ac71771f2576d04148ff0aef64e426a3ee8ec1f7
Thalysson Emanoel <thalysson140105@gmail.com>
2025-09-10 14:42:10 -0400

    FEAT: Otimizei a tela de metas, fim. Por enquanto.

--- src/components/nav-menu.jsx
+++ src/components/nav-menu.jsx
@@ -5,10 +5,8 @@ import ButtonC from './Custom-Button';
 const Header = memo(({ onLoginClick }) => {
   return (
     <nav className='bg-secondary border-b-2 border-solid border-b-tertiary flex justify-between items-center w-full h-24'>
-      <div className="flex items-center">
-        <a href="/introduction" className="no-underline text-black text-3xl ml-[184px]">
+      <div className="flex items-center no-underline text-black text-3xl font-semibold ml-[184px]">
           Financial Record
-        </a>
       </div>
       <div className='mr-[184px]'>
         <ButtonC texto="Login" largura="110px" altura="34px" onClick={onLoginClick} />
